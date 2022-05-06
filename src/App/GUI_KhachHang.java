5e34977684a0393b581868d5159c6999f72e627c
thanhdat77 <tdat772001@google.com>
2022-05-06 12:22:54 +0700

    merge voi dat

---
+++ src/App/GUI_KhachHang.java
@@ -0,0 +1,14 @@
+package App;
+
+import java.sql.SQLException;
+
+import javax.swing.SwingUtilities;
+
+import GUI.FrmKhachHang;
+
+
+public class GUI_KhachHang {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
